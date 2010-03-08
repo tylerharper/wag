f787a9f1d595cecf24112e9d121c7514c45d675d
knobe <knobe.sterling@gmail.com>
2010-03-08 14:00:03 -0500

    setup.py finally works like it should

--- setup.py
+++ setup.py
@@ -1,39 +1,30 @@
 from distribute_setup import use_setuptools; use_setuptools()
 from setuptools import setup, find_packages
 
+
+
+
 setup(
     name='wag',
     version='0.1',
     description="Tail your rss and atom feeds",
     long_description="Make your tail happy, wag it!",
-    author="Tyler Harper",
-    author_email="tyler@cowboycoding.org",
     url='http://github.com/knobe/wag',
     license='BSD',
+    author="Tyler Harper",
+    author_email="tyler@cowboycoding.org",
     packages= find_packages(exclude=['distribute_setup']),
-    install_requires=[
-        'Jinja2>=2',
-        'argparse>=1',
-        'feedparser>=4.1',
-    ],
-    extras_require={'html2markdown filter':'html2text>=2'},
-    entry_points={
-        'console_scripts': [
-            'wag = wag.wag:main'
-        ],
-    },
-    zip_safe=False,
-    include_package_data=True,
-    package_data={
-    '':['*.py'],
-    'wag':['templates/*'],
-    },
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'Operating System :: POSIX',
+        'Operating System :: OS Independent',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-   ]
+    ],
+    install_requires=['Jinja2>=2', 'argparse>=1', 'feedparser>=4.1'],
+    extras_require={'html2markdown filter':'html2text>=2'},
+    entry_points={
+        'console_scripts': ['wag = wag.wag:main'],
+    },
 )
