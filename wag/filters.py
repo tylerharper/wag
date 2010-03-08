1bfab72e8d4d948cadbced7284d6a66db6ec8cdd
knobe <knobe.sterling@gmail.com>
2010-03-08 09:26:52 -0500

    fixed filter and setup.py

--- wag/filters.py
+++ wag/filters.py
@@ -1,7 +1,8 @@
 from templates import jinja_env
-from html2text import html2text
+import html2text
 
-def html_to_markdown(value):
-    return html2text(value)
+def html_to_markdown(value, width=70):
+    html2text.BODY_WIDTH = width
+    return html2text.html2text(value)
 
 jinja_env.filters['html2markdown'] = html_to_markdown
