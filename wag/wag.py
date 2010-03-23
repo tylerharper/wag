d7b7317a13dca33de7c279b8fbd9a628a834e4c6
knobe <knobe.sterling@gmail.com>
2010-03-23 00:09:27 +0000

    ok it should be fine

--- wag/wag.py
+++ wag/wag.py
@@ -19,8 +19,8 @@ def multi_feed(func):
             if self.args.template is None:
                 self.args.template = template
              
-            feed = feedparser.parse(url).entries
-            feed.reverse()
+            feed = feedparser.parse(url)
+            feed.entires.reverse()
     
             return func(self, feed)
     return pseudo_object
@@ -32,15 +32,15 @@ class Wag(object):
 
     def display_feed(self, feed):
         """Takes a list of entries from config"""
-        number_of_entries = len(entries)
+        number_of_entries = len(feed)
         if number_of_entries == 0:
-            print "There are zero feeds for %s with url %s" % (args.name, args.url)
+            print "There are zero feeds for %s with url %s" % (self.args.name, self.args.url)
             sys.exit(1)
 
         try:
-            print get_rendered_string(args.template, entries[number_of_entries-args.lines:])
+            print get_rendered_string(self.args.template, feed[number_of_entries-self.args.lines:])
         except TemplateNotFound:
-            print "%s does not appear to be a valid template in your template path" % args.template
+            print "%s does not appear to be a valid template in your template path" % self.args.template
             sys.exit(1)
         except AttributeError:
             print "Either invalid url or invalid name"
@@ -52,11 +52,11 @@ class Wag(object):
     @mutli_feed
     def show_keys(self, feed):
 
-        for k in entries[0]:
+        for k in feed[0]:
             key_str = "'%s'" % str(k)
             #this was stolen from rson
-            if args.verbose is True:
-                key_str += " => '%s'" % entries[0][k]
+            if self.args.verbose is True:
+                key_str += " => '%s'" % feed[0][k]
         
             print key_str
 
@@ -64,7 +64,7 @@ class Wag(object):
     
     def list(self):
         config_file = ConfigParser.RawConfigParser()
-        config_file.read(args.config)
+        config_file.read(self.args.config)
         for section in config_file.sections():
             print "'" + section + "'"
         sys.exit()
@@ -73,11 +73,11 @@ class Wag(object):
     def follow(self, feed):
         self.display_feed(feed)
         try:
-            last_entry = entries[-1]
+            last_entry = feed[-1]
             while True:
-                time.sleep(args.sleep_interval)
+                time.sleep(self.args.sleep_interval)
                 
-                new_entries = feedparser.parse(args.url).entries
+                new_entries = feedparser.parse(self.args.url).entries
                 
                 pos = len(new_entries)
                 for entry in new_entries:
@@ -89,7 +89,7 @@ class Wag(object):
                 new_entries.reverse()
                 
                 try:
-                    rendered_string = get_rendered_string(args.template, new_entries[pos:])
+                    rendered_string = get_rendered_string(self.args.template, new_entries[pos:])
                     if rendered_string != '':
                         print rendered_string
                 except IndexError:
@@ -101,6 +101,6 @@ class Wag(object):
             sys.exit() 
         
         except IndexError:
-            print "%s has no entries or is an invalid url" % args.url
+            print "%s has no entries or is an invalid url" % self.args.url
             sys.exit(1)
 
